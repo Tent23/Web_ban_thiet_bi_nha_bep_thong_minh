9e4c1d1f8112155aa3569d82d148a0bdec29b6bc
Le Thanh Truong <23130356@st,hcmuaf.edu.vn>
2025-11-08 13:24:10 +0700

    reup

--- pages/ThanhTruong/tuyendung.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.job-item-header').forEach(button => {
-    button.addEventListener('click', () => {
-        const item = button.closest('.job-item');
-        item.classList.toggle('active');
-    });
-});
