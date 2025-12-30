80eccf3e7da894a807df4d126a91bad49f29d5fa
Bao <23130024@st.hcmuaf.edu.vn>
2025-12-31 02:01:05 +0700

    Merge trang Bao Hành

---
+++ src/main/java/com/webthietbibep/dao/WarrantyDao.java
@@ -0,0 +1,35 @@
+package com.webthietbibep.dao;
+
+import com.webthietbibep.model.Warranty;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class WarrantyDao extends BaseDao {
+    static Map<Integer,Warranty> data = new HashMap<>();
+
+    public List<Warranty> getSearchListWarranty(String type , String pra) {
+        return get().withHandle(h ->{
+            if(type == null){
+                return new ArrayList<>();
+            }
+            else if(type.equals("phone")) {
+                return h.createQuery("select w.series , p.name as productName,w.product_id as productId,CONCAT(u.last_name , ' ' , u.first_name) as customerName, w.customer_id as customerId,w.order_id as orderId , u.phone as phoneNumber , w.purchase_date as purchaseDate, w.warranty_time  \n" +
+                        "                        from warranties w join users u on u.user_id = w.customer_id \n" +
+                        "                                                 join products p on p.product_id = w.product_id\n" +
+                        "                                                 \n" +
+                        "                        where u.phone = :pra; ").bind("pra", pra.trim()).mapToBean(Warranty.class).list();
+            }
+            else{
+
+                return h.createQuery("select w.series , p.name as productName,w.product_id as productId,CONCAT(u.last_name , ' ' , u.first_name) as customerName, w.customer_id as customerId,w.order_id as orderId , u.phone as phoneNumber , w.purchase_date as purchaseDate, w.warranty_time  \n" +
+                        "                        from warranties w join users u on u.user_id = w.customer_id \n" +
+                        "                                                 join products p on p.product_id = w.product_id\n" +
+                        "                                                 \n" +
+                        "                        where w.series = :pra;").bind("pra",pra.trim()).mapToBean(Warranty.class).list();
+            }
+        });
+    }
+}
