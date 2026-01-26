417cf9f2d3af828d9c2cb1f4f4f5834b6b88cfe2
Tent23 <23130335@st.hcmuaf.edu.vn>
2026-01-27 00:48:35 +0700

    thêm chức năng thêm nhiều ảnh cho 1 sp

--- src/main/java/com/webthietbibep/controller/ProductDetailServlet.java
+++ src/main/java/com/webthietbibep/controller/ProductDetailServlet.java
@@ -2,10 +2,12 @@ package com.webthietbibep.controller;
 
 import com.webthietbibep.dao.ProductDAO;
 import com.webthietbibep.dao.ProductCommentDAO;
+import com.webthietbibep.dao.ProductImageDAO;
 import com.webthietbibep.model.ProductComment;
 import com.webthietbibep.dao.ProductFeatureDAO;
 import com.webthietbibep.model.Product;
 import com.webthietbibep.model.ProductFeature;
+import com.webthietbibep.model.ProductImage;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -21,6 +23,7 @@ public class ProductDetailServlet extends HttpServlet {
     private final ProductDAO productDAO = new ProductDAO();
     private final ProductFeatureDAO featureDAO = new ProductFeatureDAO();
     private final ProductCommentDAO commentDAO =  new ProductCommentDAO();
+    private final ProductImageDAO imageDAO = new ProductImageDAO();
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -52,6 +55,9 @@ public class ProductDetailServlet extends HttpServlet {
 
         List<ProductComment> comments = commentDAO.getByProductId(productId);
 
+        List<ProductImage> images = imageDAO.getByProductId(productId);
+
+        request.setAttribute("images", images);
         request.setAttribute("comments", comments);
         request.setAttribute("product", product);
         request.setAttribute("features", features);
